5b759ad3bd3de46b352bfa8ff32b2753ae2d0c10
Mohcine Lmariouh <mohssinelmariouh@gmail.com>
2021-04-09 20:34:28 +0200

    45 - instalacion de un paquete colores comando => npm i colors , incluir dicha paquete , crear un metodo async me permite dentro de ella await , esta agora todo nuestro codigo va tentrdo de este metodo podemos decir que nuestra app de momento es asincrona

---
+++ app.js
@@ -0,0 +1,16 @@
+require('colors');
+console.clear();
+
+
+
+
+/* voy a tener que trabajar con varios proceso asyncronos , hay muchas formas de hacer esto en  una app de node , sabemos que todo se recorre por defecto secuencialmente , si vamos a crear las siguiente Function 
+   main que va ser asyncrono , asi todo codigo que vamos a implementar va estar dentro de main ..*/
+
+
+
+  const main = async()=>{
+     console.log('Hola mundo ');
+  }
+
+  main();
